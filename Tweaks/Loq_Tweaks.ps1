f4e95b8f0ed08635298b1da85dd485caf4a1a609
SillyCatDev <100283589+SillyCatDev@users.noreply.github.com>
2022-06-11 13:15:54 +0530

    LoQ _Tweaks.ps1 added

---
+++ Tweaks/Loq_Tweaks.ps1
@@ -0,0 +1,19 @@
+Set-Location %temp%
+mkdir TemporaryDownloads
+"This script will temporarily download executable binaries from other locations to provide it's services"
+"The script will download the following (listed in order of execution): "
+"[1] ExplorerPatcher"
+"[2] O&O ShutUp"
+Pause
+Invoke-WebRequest -Uri "https://github.com/valinet/ExplorerPatcher/releases/download/22000.708.46.5_ae40366/ep_setup.exe"
+Invoke-WebRequest -Uri "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe"
+"The required files were downloaded, to execute the files"
+Pause
+./ep_setup.exe
+"Waiting for explorer patcher to install and do its thingz... (30 seconds timeout)"
+Start-Sleep -s 30
+"Executing O&O ShutUp: Configure the settings yourself in the window that will pop-up."
+./OOSU10.exe
+"Done!"
+"To exit:"
+Pause
